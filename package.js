2c9ec152345c2fdb9db22df35e3bef3065216f75
Erik Olofsson <erik.olofsson@favro.com>
2021-07-16 12:46:58 +0200

    Update Metoor

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@3.0.3');
+  api.versionsFrom('METEOR@3.4');
 
   api.use('blaze@3.0.0');
   api.use('underscore');
@@ -27,7 +27,7 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@3.0.3');
+  api.versionsFrom('METEOR@3.4');
 
   api.use('iron:dynamic-template');
   api.use('templating');
